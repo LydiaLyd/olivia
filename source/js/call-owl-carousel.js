e78c9bca88d798440b37f2e24fe23030f7da5988
Lydia <ridea@bk.ru>
2016-05-18 20:35:35 +0400

    Add style

---
+++ source/js/call-owl-carousel.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".slider--screenshots").owlCarousel({
+    items: 4,
+    margin: 30
+  });
+  $(".slider--testimonials").owlCarousel({
+    items: 1,
+    smartSpeed: 500
+  });
+  $(".slider--blog-preview").owlCarousel({
+    items: 1,
+    smartSpeed: 500
+  });
+});
