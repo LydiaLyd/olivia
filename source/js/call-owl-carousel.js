ab4d7084a801f8ba98e8e511bc9ebe32160dba92
Lydia <ridea@bk.ru>
2016-06-01 22:05:31 +0400

    Add style for medium screens

--- source/js/call-owl-carousel.js
+++ source/js/call-owl-carousel.js
@@ -1,26 +1,34 @@
 $(document).ready(function(){
   $(".slider--screenshots").owlCarousel({
-    items: 4,
     responsiveClass: true,
     responsive: {
-        0: {
-            items: 1,
-            nav: true,
-            dots: false,
-            margin: 10
-        },
-        573: {
-          items: 2,
-          margin: 15
-        },
-        768: {
-            items: 3,
-            margin: 30,
-        },
-        1200: {
-            items: 4,
-            margin: 30,
-        }
+      0: {
+        items: 2,
+        autoWidth: true,
+        margin: 13,
+        center: true,
+        loop: true,
+        dots: false,
+      },
+      544: {
+        items: 2,
+        autoWidth: true,
+        margin: 13,
+        center: true,
+        loop: true,
+        dots: true,
+      },
+      768: {
+        items: 4,
+        autoWidth: true,
+        margin: 20,
+        center: true,
+        loop: true,
+      },
+      1200: {
+        items: 4,
+        margin: 30,
+      }
     }
   });
   $(".slider--testimonials").owlCarousel({
@@ -30,7 +38,15 @@ $(document).ready(function(){
   });
   $(".slider--blog-preview").owlCarousel({
     items: 1,
-    margin: 100,
-    smartSpeed: 500
+    smartSpeed: 500,
+    responsiveClass: true,
+    responsive: {
+      768: {
+        margin: 30,
+      },
+      1200: {
+        margin: 130,
+      },
+    }
   });
 });
